f4c2a3dfc66a8eb0cf46a5e6c9bdbb5c7f5cb645
Rafael <rafael.raffy.agas@gmail.com>
2023-06-12 17:28:26 -0400

    fixing errors

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -105,7 +105,7 @@ class Question(models.Model):
     # Foreign key to lesson
     course = models.ForeignKey(Course, on_delete = models.CASCADE)
     # question text
-    question = models.CharField(max_length= 200)
+    content = models.CharField(max_length= 200)
     # question grade/mark
     grade = models.IntegerField(default = 10)
 
